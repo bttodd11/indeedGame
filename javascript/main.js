e8c19dfa4ec661b2afdfd9f0b1dbb4aef13b3a0c
bttodd11 <bttodd@icloud.com>
2020-01-31 16:01:20 -0600

    start/stop working plus score

--- javascript/main.js
+++ javascript/main.js
@@ -1,124 +1,107 @@
 var score = 0;
-var color = "blue";
+var int;
+const velocity = 0;
 
-
-
-
-function random(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-function setBG() {
-    if (Math.round(Math.random())) {
-        return "icons/bigPaper.png";
-    }
-}
-
-//random start for animation
-document.getElementById("start").onclick = function () { startGame() };
-function startGame(){
-    for (i = 0; i < 10; i++) {
-        dropBox();
-    }
-    var runGame = setInterval(function () {
-        for (i = 0; i < 5; i++) {
+const startGame = () => {
+    int = setInterval(function(){
+        for (i = 0; i < 6; i++) {
             dropBox();
         }
-    }, 1000);
+    }
+    ,4000)
 }
-function pauseWindow() {
-    var txt;
-    var person = prompt("Adjust your speed");
 
-  }
+document.getElementById("start").onclick = function(){startGame()};
+document.getElementById("stop").onclick = function(){pauseGame()};
 
+function random(min,max){
+ 	return Math.round(Math.random() * (max-min) + min);
+}
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("value");
-
-
-
-
-//Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.innerHTML = this.value;
+function setBG(){
+  if (Math.round(Math.random())){
+    return "icons/bigPaper.png";
+  } 
 }
 
 
+const pauseGame = () => {
+    clearInterval(int);
+}
 
-function dropBox() {
-    console.log("call")
-    var length = random(100, ($(".game").width() - 100));
-    var velocity = 4000;
-    var size = random(10, 100);
-    var thisBox = $("<div/>", {
-        class: "box",
-        style: "width:" + size + "px; height:" + size + "px; left:" + length + "px; transition: transform " + velocity + "ms linear;"
-    });
-
-    //set data and bg based on data
-    thisBox.data("test", Math.round(Math.random()));
-    if(thisBox.data("test")){
-      thisBox.css({"background": "url('http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/128/Frankenstein-icon.png')", "background-size":"contain"});
-    } else {
-      thisBox.css({"background": "url('icons/bigPaper.png')", "background-size":"contain"});
-    }
-
-
-    //insert gift element
-    $(".game").append(thisBox);
-        setTimeout(function(){
-            thisBox.addClass("move");
-        },random(0, 5000))
-    //remove this object when animation is over
-    thisBox.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
-        function (event) {
-            $(this).remove();
-        });
-    }
-
-
-
-
-$(document).on('click', '.box', function () {
-    // if ($(this).data("test")) {
-    //     score += 1;
-    // } else {
-    //     score -= 1;
-    // }
-
-    // $(".score").html(score);
+function dropBox(){
+  var length = random(100, ($(".game").width() - 100));
+  const velocity = 3500;
+  var size = random(10, 100);
+  var thisBox = $("<div/>", {
+    class: "box",
+    style:  "width:" +size+ "px; height:"+size+"px; left:" + length+  "px; transition: transform " +velocity+ "ms linear;"
+  });
+  
+  //set data and bg based on data
+  thisBox.data("test", Math.round(Math.random()));
+  if(thisBox.data("test")){
+    thisBox.css({"background": "url('icons/bigPaper.png')", "background-size":"contain"});
+  } 
+  
+  
+  //insert gift element
+  $(".game").append(thisBox);
+  
+  //random start for animation
+   setTimeout(function(){
+    thisBox.addClass("move");
+  }, random(0, 3000));
+  
+  //remove this object when animation is over
+  thisBox.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+              function(event) {
     $(this).remove();
+  });
+}
+
+// function start(){
+// var runGame = setInterval(run, 10);
+// var stop = document.getElementById("stop").onclick = function(){start()};
+// if(stop){
+//     clearInterval(runGame)
+// }
+// function run(){
+//     for (i = 0; i < 3; i++) { 
+//         dropBox();
+//     }
+// }
+// }
+
+$(document).on('click', '.box', function(){
+  if($(this).data("test")){
+    score += 1;
+    console.log(score)
+  } 
+
+  $(".score").html(score);
+  $(this).remove();
 });
 
 
-
-// var runGame = setInterval(function () {
-//     for (i = 0; i < 10; i++) {
-//         dropBox();
-//     }
-// }, 3000);
-
-
-
 function countdown() {
-    var seconds = 60;
-    function tick() {
-        var counter = document.getElementById("counter");
-        seconds--;
-        counter.innerHTML = (seconds < 10 ? "0" : "") + String(seconds) + "S";
-            setTimeout(tick, 1000);
-      
- 
-            // alert("Game over");
-            // clearInterval(runGame);
-        }
-    tick();
-}
-
-
+    	var seconds = 90;
+	    function tick() {
+	        var counter = document.getElementById("counter");
+	        seconds--;
+	        counter.innerHTML = (seconds < 10 ? "0" : "")  + String(seconds) + "S";
+	        if( seconds > 0 ) {
+	            setTimeout(tick, 1000);
+	        } else {
+	            alert("Game is paused");
+	            clearInterval(runGame);
+	        }
+	    }
+    	tick();
+	}
 
 countdown();
+
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -132,6 +115,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  pauseGame()
 }
 
 // When the user clicks on <span> (x), close the modal
