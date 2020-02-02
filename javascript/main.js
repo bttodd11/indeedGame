e5fc0b11e8072ffdb7a7a7959c69d9a3e74c3479
bttodd11 <bttodd@icloud.com>
2020-02-02 16:18:52 -0600

    Functionality done for the most part

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,33 @@
 //  Starting variables
 var score = 0;
 var int;
-const velocity = 0;
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
+var velocity = 5000;
+
+
+$(document ).ready(function() {
+    // $("#instructions").fadeOut(9000)
+});
+
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+
+
 
 // The function to start the setInterval that will start the 
 // game, This will drop 6 applicants every 4 seconds.
 const startGame = () => {
     int = setInterval(function () {
-        for (i = 0; i < 6; i++) {
+        for (i = 0; i < 5; i++) {
             applicantDrop();
         }
     }
-        , 3000)
+        , 4000)
 }
 
 // Pausing game that will clear the previous setInterval
@@ -19,7 +35,7 @@ const pauseGame = () => {
     clearInterval(int);
 }
 
-// Randomizer for size of icons between chosen px (10-100)
+// Randomizer for needed functions
 function random(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
@@ -31,8 +47,9 @@ function random(min, max) {
 function applicantDrop(){
 // Creating the box for the applicants
   var length = random(100, ($(".game").width() - 100));
-  const velocity = 3500;
-  var applicantSize = random(10, 100);
+  var velocity = document.getElementById("myRange").value * 100;
+  console.log(velocity)
+  var applicantSize = random(30, 100);
   var thisBox = $("<div/>", {
     class: "box",
     style:  "width:" + 
@@ -42,10 +59,18 @@ function applicantDrop(){
     velocity+ "ms linear;"
   });
   
-  thisBox.data("applicants", Math.round(Math.random()));
-  if(thisBox.data("applicants")){
-    thisBox.css({"background": "url('icons/bubble.png')", "background-size":"contain"});
-  } 
+    thisBox.data("applicants", Math.round(Math.random() * 5));
+    if (thisBox.data("applicants") == 1) {
+        thisBox.css({ "background": "url('icons/teacher.png')", "background-size": "contain" });
+    } else if (thisBox.data("applicants") == 2) {
+        thisBox.css({ "background": "url('icons/nursef.png')", "background-size": "contain" });
+    }
+    else if (thisBox.data("applicants") == 3) {
+        thisBox.css({ "background": "url('icons/nurse.png')", "background-size": "contain" });
+    }
+    else if (thisBox.data("applicants") == 4) {
+        thisBox.css({ "background": "url('icons/doctor.png')", "background-size": "contain" });
+    }
   
   
     // Insert applicant to the game
@@ -53,7 +78,8 @@ function applicantDrop(){
     setTimeout(function () {
         thisBox.addClass("move");
     }, random(0, 3000));
-  
+
+    
 
     // If the applicant reachs the botton of the length remove 
     // The applicant from the screen
@@ -64,34 +90,39 @@ function applicantDrop(){
 }
 
 
-$(document).on('click', '.box', function(){
-  if($(this).data("applicants")){
-    score += 1;
-    console.log(score)
-  } 
-
-  $(".score").html(score);
-  $(this).remove();
+$(document).on('click', '.box', function () {
+    var size = $(this).width()
+    var finder = $(this)
+    if ($(this).data("applicants")) {
+        if (size > 90) {
+            score += 1;
+        }
+        else if (size <= 90 && size >= 60) {
+            score += 3;
+        }
+        else if (size < 60){
+            score += 10;
+        }
+    }
+    $(".score").html(score);
+    $(this).remove();
 });
 
 
-function countdown() {
-    	var seconds = 90;
-	    function tick() {
-	        var counter = document.getElementById("counter");
-	        seconds--;
-	        counter.innerHTML = (seconds < 10 ? "0" : "")  + String(seconds) + "S";
-	        if( seconds > 0 ) {
-	            setTimeout(tick, 1000);
-	        } else {
-	            alert("Game is paused");
-	            clearInterval(runGame);
-	        }
-	    }
-    	tick();
-	}
-
-countdown();
+function clock() {
+    var seconds = 120;
+    function count() {
+        seconds--;
+        if (seconds > 0) {
+            setTimeout(count, 1000);
+        } else {
+            alert("Game over");
+            clearInterval(startGame);
+        }
+    }
+    count();
+}
+clock();
 
 // jQuery needed for onClicks and Modal
 
@@ -119,18 +150,20 @@ document.getElementById("pause").onclick = function() {
   modal.style.display = "block";
   pauseGame();
   $("#pause").show()
-
-
 }
-
-document.getElementsByClassName("box").onclick = function () {
-    if ($(this).data("applicants")) {
-        score += 1;
-        console.log(score)
-    }
+document.getElementById("resume").onclick = function() {
+    modal.style.display = "none";
+    startGame();
+  }
+document.getElementById("reset").onclick = function() {
+    modal.style.display = "none";
+    clock()
+    startGame()
+    score = 0
     $(".score").html(score);
-    $(this).remove();
-}
+    
+  }
+
 
 
 // When the user clicks on <span> (x), close the modal
