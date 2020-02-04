b0752314d00223dd85a79b70b20a7f279a183643
Todd <brian-todd@siemens.com>
2020-02-04 01:22:36 -0600

    Score Working and applicants creation change

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,8 @@
 //  Starting variables
 let score = 0;
 let int;
+let timeElasped;
+let thisBox;
 const slider = document.getElementById("myRange");
 const output = document.getElementById("value");
 const modal = document.getElementById("pauseWindow");
@@ -8,7 +10,12 @@ const btn = document.getElementById("pause");
 const span = document.getElementsByClassName("close")[0];
 const toggle = document.getElementById("toggle");
 const reset = document.getElementById("reset");
-const game = document.getElementsByClassName("game");
+const game = document.querySelector(".game");
+const gameWidth = game.clientWidth;
+const gameHeight = game.clientHeight;
+let gameRaf,
+start = null,
+last = 0
 
 output.innerHTML = slider.value;
 let velocity = 5000;
@@ -19,55 +26,85 @@ slider.oninput = () => {
   }
 
 
+const animateApplicant = () => {
+    var animateTime = slider.value/ 60 + 'px',
+    applicantPosition = parseInt($('.box').css('top'));
+
+    if(applicantPosition >= gameHeight){
+        $('.box:first-child').remove();
+    } else {
+        $('.box').css({
+            top: '+=' + animateTime,
+        });
+    }
+};
+
+gameTimer = function(timestamp){
+    gameRaf = window.requestAnimationFrame(gameTimer);
+    animateApplicant();
+    timeElasped = Math.floor(timestamp / 1000);
+    if (!last || timestamp - last >= 1000){
+        var progress = timestamp - start;
+        last = timestamp;
+        createApplicant();
+    }
+};
+
 
 
 // The function to start the setInterval that will start the 
 // game, This will drop 6 applicants every 4 seconds.
 const startGame = () => {
-    int = setInterval(function () {
-        for (i = 0; i < 5; i++) {
-            applicantDrop();
-        }
-    }
-        , 4000)
-}
+    gameRaf = requestAnimationFrame(gameTimer);
+    clock();
+};
 
 // Pausing game that will clear the previous setInterval
 const pauseGame = () => {
-    clearInterval(int);
+   cancelAnimationFrame(gameRaf);
 }
-
-// Randomizer for needed functions
-const random = (min, max) => {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
 
 
 // The function that will drop the applicants with a random size
 // and a chosen speed
-const applicantDrop = () => {
+const createApplicant = () => {
     // Creating the box for the applicants
-    const length = random(100, ($(".game").width() - 100));
-    let velocity = document.getElementById("myRange").value * 100;
-    let applicantSize = random(30, 100);
-    let thisBox = document.createElement("div");
-    thisBox.class = "box";
-    thisBox.style =  "width:" + applicantSize + "px;" + "height:" + applicantSize + "px;" + "left:" + length + "px;" + "transition: transform " + velocity + "ms linear;";
-    
+    let applicantSize = Math.floor(Math.random() * 100) + 10;
+    let leftPos = Math.floor(Math.random() * gameWidth);
+    let thisBox = document.createElement('div');
+    const randomIcon = Math.round(Math.random() * 5);
+    let yPos;
 
-    thisBox.applicants = Math.round(Math.random() * 5);
-    switch (thisBox.applicants) {
+    if(leftPos + applicantSize > gameWidth){
+        left = leftPos - applicantSize;
+    } else if (leftPos - applicantSize < 0) {
+        left = leftPos + applicantSize;
+    }
+    
+    thisBox.className = 'box';
+    thisBox.style = 
+        'width:' + 
+        applicantSize +
+        'px;' +
+        'height:' +
+        applicantSize +
+        'px;' +
+        'left:' +
+        leftPos +
+        'px;' +
+        'top:' +
+        yPos +
+        'px;';
+
+    switch (randomIcon) {
         case 0:
             thisBox.style.background = "url('icons/teacher.png')";
             thisBox.style.backgroundSize = "contain";
             break;
-
         case 1:
             thisBox.style.background = "url('icons/nurse.png')";
             thisBox.style.backgroundSize = "contain";
             break;
-        
         case 2:
             thisBox.style.background = "url('icons/nursef.png')";
             thisBox.style.backgroundSize = "contain";
@@ -76,45 +113,82 @@ const applicantDrop = () => {
         case 3:
             thisBox.style.background = "url('icons/doctor.png')";
             thisBox.style.backgroundSize = "contain";
-            break;
+            break; 
     }
-  
-  
-    // Insert applicant to the game
-    game[0].appendChild(thisBox);
+    game.appendChild(thisBox);
+    thisBox.addEventListener('click', () => scoreSub())
 
-    setTimeout(function () {
-        thisBox.classList.add("move");
-    }, random(0, 3000));
+  
+    const scoreSub = () => {
+        var value = thisBox.style.width.substring(0, thisBox.style.width.length - 3);
+  
+    
+     switch (value) {
+         case "1":
+             score += 10;
+             break;
 
+         case "2":
+             score += 9;
+             break;
+
+         case "3":
+             score += 8;
+             break;
+
+         case "4":
+             score += 7;
+             break;
+
+         case "5":
+             score += 6;
+             break;
+
+         case "6":
+             score += 5;
+             break;
+
+         case "7":
+             score += 4;
+             break;
+
+         case "8":
+             score += 3;
+             break;
+         case "9":
+             score += 2;
+             break;
+         case "10":
+             score += 1;
+             break;
+
+     }
+     $(".score").html(score);
+     thisBox.remove()
+    };
+}
+
+  
+
+ 
+
+const applicantDrop = (topPos) => {
+    const length = random(100, ($(".game").width() - 100));
+    let velocity = document.getElementById("myRange").value * 100;
+
+    thisBox.style.transform = 'translateY(' + topPos + 'px)';
 
     'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend'
-    .split(' ')
-    .forEach((eventName) => {
-      thisBox.addEventListener(eventName, () => thisBox.remove(), { once: true });
-    });
-
+        .split(' ')
+        .forEach((eventName) => {
+            thisBox.addEventListener(eventName, () => thisBox.remove(), { once: true });
+        });
 }
 
 
 
-$(document).on('click', '.box', function () {
-    var size = $(this).width()
-    var finder = $(this)
-    if ($(this).data("applicants")) {
-        if (size > 90) {
-            score += 1;
-        }
-        else if (size <= 90 && size >= 60) {
-            score += 3;
-        }
-        else if (size < 60){
-            score += 10;
-        }
-    }
-    $(".score").html(score);
-    $(this).remove();
-});
+
+
 
 
 const clock = () => {
